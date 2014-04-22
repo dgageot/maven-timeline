1c2fdd3e5355866cf94e660b722f3fdca5e3ef1b
David Gageot <david@gageot.net>
2014-04-22 10:22:34 +0200

    Reformat code

--- src/main/java/net/gageot/maven/buildevents/BuildEventListener.java
+++ src/main/java/net/gageot/maven/buildevents/BuildEventListener.java
@@ -26,108 +26,110 @@ import com.github.jknack.handlebars.*;
 import com.github.jknack.handlebars.context.*;
 
 public class BuildEventListener extends AbstractExecutionListener {
-	private final File output;
-	private final Map<String, Long> startTimes = new ConcurrentHashMap<String, Long>();
-	private final Map<String, Long> endTimes = new ConcurrentHashMap<String, Long>();
+  private final File output;
+  private final Map<String, Long> startTimes = new ConcurrentHashMap<String, Long>();
+  private final Map<String, Long> endTimes = new ConcurrentHashMap<String, Long>();
 
-	public BuildEventListener(File output) {
-		this.output = output;
-	}
+  public BuildEventListener(File output) {
+    this.output = output;
+  }
 
-	@Override
-	public void mojoStarted(ExecutionEvent event) {
-		MojoExecution mojo = event.getMojoExecution();
-		String goal = mojo.getGoal();
-		String phase = mojo.getLifecyclePhase();
-		String project = event.getProject().getArtifactId();
+  @Override
+  public void mojoStarted(ExecutionEvent event) {
+    MojoExecution mojo = event.getMojoExecution();
+    String goal = mojo.getGoal();
+    String phase = mojo.getLifecyclePhase();
+    String project = event.getProject().getArtifactId();
 
-		startTimes.put(project + "/" + phase + "/" + goal, System.currentTimeMillis());
-	}
+    startTimes.put(project + "/" + phase + "/" + goal, System.currentTimeMillis());
+  }
 
-	@Override
-	public void mojoSkipped(ExecutionEvent event) {
-		mojoEnd(event);
-	}
+  @Override
+  public void mojoSkipped(ExecutionEvent event) {
+    mojoEnd(event);
+  }
 
-	@Override
-	public void mojoSucceeded(ExecutionEvent event) {
-		mojoEnd(event);
-	}
+  @Override
+  public void mojoSucceeded(ExecutionEvent event) {
+    mojoEnd(event);
+  }
 
-	@Override
-	public void mojoFailed(ExecutionEvent event) {
-		mojoEnd(event);
-	}
+  @Override
+  public void mojoFailed(ExecutionEvent event) {
+    mojoEnd(event);
+  }
 
-	private void mojoEnd(ExecutionEvent event) {
-		MojoExecution mojo = event.getMojoExecution();
-		String goal = mojo.getGoal();
-		String phase = mojo.getLifecyclePhase();
-		String project = event.getProject().getArtifactId();
+  private void mojoEnd(ExecutionEvent event) {
+    MojoExecution mojo = event.getMojoExecution();
+    String goal = mojo.getGoal();
+    String phase = mojo.getLifecyclePhase();
+    String project = event.getProject().getArtifactId();
 
-		endTimes.put(project + "/" + phase + "/" + goal, System.currentTimeMillis());
-	}
+    endTimes.put(project + "/" + phase + "/" + goal, System.currentTimeMillis());
+  }
 
-	@Override
-	public void sessionEnded(ExecutionEvent event) {
-		report();
-	}
+  @Override
+  public void sessionEnded(ExecutionEvent event) {
+    report();
+  }
 
-	public void report() {
-		long buildStartTime = Long.MAX_VALUE;
-		for (Long start : startTimes.values()) {
-			buildStartTime = Math.min(buildStartTime, start);
-		}
+  public void report() {
+    long buildStartTime = Long.MAX_VALUE;
+    for (Long start : startTimes.values()) {
+      buildStartTime = Math.min(buildStartTime, start);
+    }
 
-		long buildEndTime = 0;
-		for (Long end : endTimes.values()) {
-			buildEndTime = Math.max(buildEndTime, end);
-		}
+    long buildEndTime = 0;
+    for (Long end : endTimes.values()) {
+      buildEndTime = Math.max(buildEndTime, end);
+    }
 
-		List<Measure> measures = new ArrayList<Measure>();
-		for (String key : startTimes.keySet()) {
-			String[] keyParts = key.split("/");
+    List<Measure> measures = new ArrayList<Measure>();
+    for (String key : startTimes.keySet()) {
+      String[] keyParts = key.split("/");
 
-			Measure measure = new Measure();
-			measure.project = keyParts[0];
-			measure.phase = keyParts[1];
-			measure.goal = keyParts[2];
-			measure.left = ((startTimes.get(key) - buildStartTime) * 10000L) / (buildEndTime - buildStartTime);
-			measure.width = (((endTimes.get(key) - buildStartTime) * 10000L) / (buildEndTime - buildStartTime)) - measure.left;
-			measures.add(measure);
-		}
+      Measure measure = new Measure();
+      measure.project = keyParts[0];
+      measure.phase = keyParts[1];
+      measure.goal = keyParts[2];
+      measure.left = ((startTimes.get(key) - buildStartTime) * 10000L) / (buildEndTime - buildStartTime);
+      measure.width = (((endTimes.get(key) - buildStartTime) * 10000L) / (buildEndTime - buildStartTime)) - measure.left;
+      measures.add(measure);
+    }
 
-		Collections.sort(measures);
+    Collections.sort(measures);
 
-		try {
-			String html = new Handlebars().compile("template").apply(Context.newBuilder(measures).resolver(FieldValueResolver.INSTANCE).build());
-			write(html);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    try {
+      String html = new Handlebars().compile("template").apply(Context.newBuilder(measures).resolver(FieldValueResolver.INSTANCE).build());
+      write(html);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-	private void write(String message) throws IOException {
-		File path = output.getParentFile();
-		if (!path.exists()) {
-			path.mkdirs();
-		}
+  private void write(String message) throws IOException {
+    File path = output.getParentFile();
+    if (!path.exists()) {
+      if (!path.mkdirs()) {
+        throw new IOException("Unabel to create " + path);
+      }
+    }
 
-		FileWriter writer = new FileWriter(output);
-		writer.write(message);
-		writer.close();
-	}
+    FileWriter writer = new FileWriter(output);
+    writer.write(message);
+    writer.close();
+  }
 
-	public static class Measure implements Comparable<Measure> {
-		String project;
-		String phase;
-		String goal;
-		Long left;
-		Long width;
+  public static class Measure implements Comparable<Measure> {
+    String project;
+    String phase;
+    String goal;
+    Long left;
+    Long width;
 
-		@Override
-		public int compareTo(Measure other) {
-			return left.compareTo(other.left);
-		}
-	}
+    @Override
+    public int compareTo(Measure other) {
+      return left.compareTo(other.left);
+    }
+  }
 }
--- src/main/java/net/gageot/maven/buildevents/ExecutionListenerChain.java
+++ src/main/java/net/gageot/maven/buildevents/ExecutionListenerChain.java
@@ -18,113 +18,113 @@ package net.gageot.maven.buildevents;
 import org.apache.maven.execution.*;
 
 public class ExecutionListenerChain implements ExecutionListener {
-	private final ExecutionListener first;
-	private final ExecutionListener second;
+  private final ExecutionListener first;
+  private final ExecutionListener second;
 
-	public ExecutionListenerChain(ExecutionListener first, ExecutionListener second) {
-		this.first = first;
-		this.second = second;
-	}
+  public ExecutionListenerChain(ExecutionListener first, ExecutionListener second) {
+    this.first = first;
+    this.second = second;
+  }
 
-	@Override
-	public void projectDiscoveryStarted(ExecutionEvent event) {
-		first.projectDiscoveryStarted(event);
-		second.projectDiscoveryStarted(event);
-	}
+  @Override
+  public void projectDiscoveryStarted(ExecutionEvent event) {
+    first.projectDiscoveryStarted(event);
+    second.projectDiscoveryStarted(event);
+  }
 
-	@Override
-	public void sessionStarted(ExecutionEvent event) {
-		first.sessionStarted(event);
-		second.sessionStarted(event);
-	}
+  @Override
+  public void sessionStarted(ExecutionEvent event) {
+    first.sessionStarted(event);
+    second.sessionStarted(event);
+  }
 
-	@Override
-	public void sessionEnded(ExecutionEvent event) {
-		first.sessionEnded(event);
-		second.sessionEnded(event);
-	}
+  @Override
+  public void sessionEnded(ExecutionEvent event) {
+    first.sessionEnded(event);
+    second.sessionEnded(event);
+  }
 
-	@Override
-	public void projectSkipped(ExecutionEvent event) {
-		first.projectSkipped(event);
-		second.projectSkipped(event);
-	}
+  @Override
+  public void projectSkipped(ExecutionEvent event) {
+    first.projectSkipped(event);
+    second.projectSkipped(event);
+  }
 
-	@Override
-	public void projectStarted(ExecutionEvent event) {
-		first.projectStarted(event);
-		second.projectStarted(event);
-	}
+  @Override
+  public void projectStarted(ExecutionEvent event) {
+    first.projectStarted(event);
+    second.projectStarted(event);
+  }
 
-	@Override
-	public void projectSucceeded(ExecutionEvent event) {
-		first.projectSucceeded(event);
-		second.projectSucceeded(event);
-	}
+  @Override
+  public void projectSucceeded(ExecutionEvent event) {
+    first.projectSucceeded(event);
+    second.projectSucceeded(event);
+  }
 
-	@Override
-	public void projectFailed(ExecutionEvent event) {
-		first.projectFailed(event);
-		second.projectFailed(event);
-	}
+  @Override
+  public void projectFailed(ExecutionEvent event) {
+    first.projectFailed(event);
+    second.projectFailed(event);
+  }
 
-	@Override
-	public void mojoSkipped(ExecutionEvent event) {
-		first.mojoSkipped(event);
-		second.mojoSkipped(event);
-	}
+  @Override
+  public void mojoSkipped(ExecutionEvent event) {
+    first.mojoSkipped(event);
+    second.mojoSkipped(event);
+  }
 
-	@Override
-	public void mojoStarted(ExecutionEvent event) {
-		first.mojoStarted(event);
-		second.mojoStarted(event);
-	}
+  @Override
+  public void mojoStarted(ExecutionEvent event) {
+    first.mojoStarted(event);
+    second.mojoStarted(event);
+  }
 
-	@Override
-	public void mojoSucceeded(ExecutionEvent event) {
-		first.mojoSucceeded(event);
-		second.mojoSucceeded(event);
-	}
+  @Override
+  public void mojoSucceeded(ExecutionEvent event) {
+    first.mojoSucceeded(event);
+    second.mojoSucceeded(event);
+  }
 
-	@Override
-	public void mojoFailed(ExecutionEvent event) {
-		first.mojoFailed(event);
-		second.mojoFailed(event);
-	}
+  @Override
+  public void mojoFailed(ExecutionEvent event) {
+    first.mojoFailed(event);
+    second.mojoFailed(event);
+  }
 
-	@Override
-	public void forkStarted(ExecutionEvent event) {
-		first.forkStarted(event);
-		second.forkStarted(event);
-	}
+  @Override
+  public void forkStarted(ExecutionEvent event) {
+    first.forkStarted(event);
+    second.forkStarted(event);
+  }
 
-	@Override
-	public void forkSucceeded(ExecutionEvent event) {
-		first.forkSucceeded(event);
-		second.forkSucceeded(event);
-	}
+  @Override
+  public void forkSucceeded(ExecutionEvent event) {
+    first.forkSucceeded(event);
+    second.forkSucceeded(event);
+  }
 
-	@Override
-	public void forkFailed(ExecutionEvent event) {
-		first.forkFailed(event);
-		second.forkFailed(event);
-	}
+  @Override
+  public void forkFailed(ExecutionEvent event) {
+    first.forkFailed(event);
+    second.forkFailed(event);
+  }
 
-	@Override
-	public void forkedProjectStarted(ExecutionEvent event) {
-		first.forkedProjectStarted(event);
-		second.forkedProjectStarted(event);
-	}
+  @Override
+  public void forkedProjectStarted(ExecutionEvent event) {
+    first.forkedProjectStarted(event);
+    second.forkedProjectStarted(event);
+  }
 
-	@Override
-	public void forkedProjectSucceeded(ExecutionEvent event) {
-		first.forkedProjectSucceeded(event);
-		second.forkedProjectSucceeded(event);
-	}
+  @Override
+  public void forkedProjectSucceeded(ExecutionEvent event) {
+    first.forkedProjectSucceeded(event);
+    second.forkedProjectSucceeded(event);
+  }
 
-	@Override
-	public void forkedProjectFailed(ExecutionEvent event) {
-		first.forkedProjectFailed(event);
-		second.forkedProjectFailed(event);
-	}
+  @Override
+  public void forkedProjectFailed(ExecutionEvent event) {
+    first.forkedProjectFailed(event);
+    second.forkedProjectFailed(event);
+  }
 }
